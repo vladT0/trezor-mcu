545c71db8ce62a069f0ff1c6f651fee8b4d8c0f4
vladT0 <vladimirtitov@gmail.com>
2018-06-25 13:16:32 +0200

    new file:   resource.c

---
+++ resource.c
@@ -0,0 +1,31 @@
+/*
+ * This file is part of the TREZOR project, https://trezor.io/
+ *
+ * Copyright (C) 2018 vladT0
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "resource.h"
+
+#pragma GCC diagnostic ignored "-Wunused-variable"
+
+static const char *str_do_you_want = "Do you want to";
+static const char *str_i_take_risk = "I take the risk";
+static const char *str_abort       = "Abort";
+static const char *str_continue    = "Continue";
+static const char *str_cancel      = "Cancel";
+static const char *str_confirm     = "Confirm";
+static const char *str_next        = "Next";
+
